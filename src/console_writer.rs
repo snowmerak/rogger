9950228dc6a3f4e19932fa870b8cd0c911cf71f2
snowmerak <snowmerak@outlook.com>
2025-12-26 20:56:12 +0900

    feat(console_writer): implement constructor for ConsoleWriter

--- src/console_writer.rs
+++ src/console_writer.rs
@@ -3,6 +3,12 @@ use crate::writer::Writer;
 #[derive(Debug)]
 pub struct ConsoleWriter;
 
+impl ConsoleWriter {
+    pub fn new() -> Self {
+        ConsoleWriter
+    }
+}
+
 impl Writer for ConsoleWriter {
     fn write(&mut self, line: &str) -> std::io::Result<()> {
         println!("{}", line);
